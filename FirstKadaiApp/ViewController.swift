52397dc01ea93c5079b058f7cc773164a8f8c7a3
REAS-WEB <reas_web@icloud.com>
2016-02-19 18:37:26 +0900

    はじめてのアプリ提出

---
+++ FirstKadaiApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstKadaiApp
+//
+//  Created by REAS WEB CONSULTING on 2016/02/18.
+//  Copyright © 2016年 takayuki.sato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
